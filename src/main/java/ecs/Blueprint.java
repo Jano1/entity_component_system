af5a198fa616271f6086ea3cb1b893d822dae6e8
Jan Leissner <jleissner@uos.de>
2018-03-23 19:09:10 +0100

    implemented constructor

--- src/main/java/ecs/Blueprint.java
+++ src/main/java/ecs/Blueprint.java
@@ -3,10 +3,17 @@ package ecs;
 import component.Component;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Blueprint extends ArrayList<Component> {
 
     private static Blueprint empty = new Blueprint();
+
+    public Blueprint(){ }
+
+    public Blueprint(List<Component> components) {
+        super(components);
+    }
 
     public static Blueprint empty_blueprint(){
         return empty;
